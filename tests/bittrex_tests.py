24a06d28a242bacbd5574274f38d7d3a48eb1ae1
Toni Magni <email@antoniomagni.com>
2018-08-19 17:23:59 +0200

    Add first test which doesn't work yet.

--- tests/bittrex_tests.py
+++ tests/bittrex_tests.py
@@ -4,21 +4,30 @@ Created on Aug 19, 2018
 @author: afm
 '''
 import unittest
+import engines
 
 
 class Test(unittest.TestCase):
 
 
     def setUp(self):
-        pass
+        import json
+        configFile = 'arbitrage_config.json'
+        f = open(configFile)    
+        self.config = json.load(f)
+        f.close()
+        self.config['isMockMode'] = False
+        self.config['logdata']    = '/tmp/triangular.log'
+
 
 
     def tearDown(self):
         pass
 
 
-    def testName(self):
-        pass
+    def testOne(self):
+        engine = engines.triangular_arbitrage.CryptoEngineTriArbitrage(self.config)
+
 
 
 if __name__ == "__main__":
